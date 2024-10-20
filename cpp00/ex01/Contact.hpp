ab1fb01dc05c2250854eb213a10320c134d12204
h-claude <hippolyteclaude@icloud.com>
2024-10-20 18:50:56 +0200

    j'avance

--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: hclaude <hclaude@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 00:22:51 by hclaude           #+#    #+#             */
-/*   Updated: 2024/10/20 00:37:41 by hclaude          ###   ########.fr       */
+/*   Updated: 2024/10/20 18:50:14 by hclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,28 @@
 class Contact
 {
 	public :
-		void geter();
+
+		Contact();
+		~Contact();
+		void		set_firstname(std::string firsname, Contact &contact);
+		void		set_lastname(std::string lastname, Contact &contact);
+		void		set_nickname(std::string nickname, Contact &contact);
+		void		set_phone_number(std::string phone_number, Contact &contact);
+		void		set_darkest_secret(std::string darkest_secret, Contact &contact);
+		void		remove_firstname(std::string firsname, Contact &contact);
+		void		remove_lastname(std::string lastname, Contact &contact);
+		void		remove_nickname(std::string nickname, Contact &contact);
+		void		remove_phone_number(std::string phone_number, Contact &contact);
+		void		remove_darkest_secret(std::string darkest_secret, Contact &contact);
+		void		print_contact(Contact &contact);
 
 	private :
-		std::string _first_name;
-		std::string _last_name;
-		std::string _nickname;
-		std::string _phone_number;
-		std::string _darkest_secret;
+
+		std::string	_first_name;
+		std::string	_last_name;
+		std::string	_nickname;
+		std::string	_phone_number;
+		std::string	_darkest_secret;
 };
 
 #endif
--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: hclaude <hclaude@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 00:17:52 by hclaude           #+#    #+#             */
-/*   Updated: 2024/10/20 00:23:50 by hclaude          ###   ########.fr       */
+/*   Updated: 2024/10/20 18:49:15 by hclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,21 @@
 #include "Contact.hpp"
 #include <iostream>
 #include <string>
+
+class PhoneBook
+{
+	public :
+
+		void		add_contact(PhoneBook &phonebook);
+		void		remove_contact(PhoneBook &phonebook, int index);
+		void		search_contact(PhoneBook &phonebook, int index);
+		
+	private :
+
+		int			number_of_contact;
+		int			older;
+		Contact		phonebook[8];
+};
 
 #endif
 
